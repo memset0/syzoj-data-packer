173a680625367379c761ba24ad2055a97eec6a80
memset0 <memset0@outlook.com>
2020-06-03 09:56:56 +0800

    （可选的）自动重命名避免文件名冲突

--- syzoj.js
+++ syzoj.js
@@ -2,16 +2,18 @@ const fs = require('fs');
 const rd = require('rd');
 const _ = require('lodash');
 const path = require('path');
+const stringRandom = require('string-random');
 
+const utils = require('./utils');
 const modules = {
 	'subtask': require('./modules/subtask'),
 }
 
-function main(rootPath, timeLimit = 1000, memoryLimit = 512, inputExtra = ['.in'], outputExtra = ['.out', '.ans']) {
+function main(rootPath, timeLimit = 1000, memoryLimit = 512, inputExtra = ['.in'], outputExtra = ['.out', '.ans'], rename = false) {
 	let filelist = rd.readSync(rootPath).map(o => path.relative(rootPath, o));
 	let testdata = {};
 	filelist.forEach(file => {
-		let basename = path.posix.basename(file, path.extname(file));
+		let basename = path.join(file, '..', path.posix.basename(file, path.extname(file)));
 		if (inputExtra.includes(path.extname(file))) {
 			testdata[basename] = { ...testdata[basename], input: file };
 		}
@@ -19,6 +21,21 @@ function main(rootPath, timeLimit = 1000, memoryLimit = 512, inputExtra = ['.in'
 			testdata[basename] = { ...testdata[basename], output: file };
 		}
 	});
+	if (rename) {
+		Object.values(testdata).forEach((testcase) => {
+			let key = stringRandom(8, { letters: 'ABCDEF' });
+			const generate = file => {
+				if (/.+-[0-9A-F]{8}$/.test(path.posix.basename(file, path.extname(file)))) {
+					return file
+				} else {
+					return path.join(file, '..', path.posix.basename(file, path.extname(file)) + '-' + key + path.extname(file));
+				}
+			}
+			fs.renameSync(path.resolve(rootPath, testcase.input), path.resolve(rootPath, generate(testcase.input)));
+			fs.renameSync(path.resolve(rootPath, testcase.output), path.resolve(rootPath, generate(testcase.output)));
+		})
+	}
+
 }
 
 main(path.resolve('./data'))
